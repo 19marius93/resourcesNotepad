402da94ea6cf242ee7dd7d47f41ccb623195560b
19marius93 <oceanumarius93@yahoo.com>
2022-08-16 20:36:44 +0100

    Deploy

---
+++ src/components/UI/BaseButton.vue
@@ -0,0 +1,40 @@
+<template>
+  <button :class="mode">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  props: ['mode'],
+};
+</script>
+
+<style scoped>
+button {
+  padding: 0.75rem 1.5rem;
+  font-family: inherit;
+  background-color: #eb4d42;
+  border: 1px solid #eb4d42;
+  color: white;
+  cursor: pointer;
+  border-radius: 12px;
+}
+
+button:hover,
+button:active {
+  background-color: #ee3029;
+  border-color: #270041;
+}
+
+.flat {
+  background-color: transparent;
+  color: #3b6edd;
+  border: none;
+}
+
+.flat:hover,
+.flat:active {
+  background-color: #f7c1c1;
+}
+</style>
